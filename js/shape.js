baca62235284c98ffea63324989596e75bf1e89c
Carlos Pinho <kakasp04@gmail.com>
2016-03-15 10:58:02 +0000

    added some minor changes

--- js/shape.js
+++ js/shape.js
@@ -1,77 +1,77 @@
 "use strict";
 class Shape {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
 }
 
 class SpaceShip extends Shape {
-  constructor(x, y) {
-    super(x, y);
-  }
-  drawShip(ctx, x, y) {
-    var img = new Image;
-    img.onload = function() {
-      ctx.drawImage(img, x, y, ctx.canvas.width / 10, ctx.canvas.width / 10);
+    constructor(x, y) {
+        super(x, y);
     }
-    img.src = "../resources/0002.png";
+    drawShip(ctx, x, y) {
+        var img = new Image;
+        img.onload = function() {
+            ctx.drawImage(img, x, y, ctx.canvas.width / 10, ctx.canvas.width / 10);
+        }
+        img.src = "../resources/0002.png";
 
-  }
-  renewCOORD(aux, x, y) {
-    aux.clearRect(0, 0, aux.canvas.width, aux.canvas.height);
-    this.drawShip(aux, x, y);
-  }
-  newMove(shipCtx, x, y) {
-    this.x = x;
-    this.y = y;
-    this.renewCOORD(shipCtx, x, y);
-  }
+    }
+    renewCOORD(aux, x, y) {
+        aux.clearRect(0, 0, aux.canvas.width, aux.canvas.height);
+        this.drawShip(aux, x, y);
+    }
+    newMove(shipCtx, x, y) {
+        this.x = x;
+        this.y = y;
+        this.renewCOORD(shipCtx, x, y);
+    }
 
 }
 class Shoot extends Shape {
-  constructor(x, y, r) {
-    super(x, y);
-  }
-  drawShoot(extraCtx, x, y) {
-    extraCtx.beginPath();
-    extraCtx.arc(x, y, extraCtx.canvas.width / 150, 0, 2 * Math.PI);
-    extraCtx.fillStyle = "#fa9731"
-    extraCtx.fill();
-  }
-  renewCOORDS(extraCtx, x, y) {
-    this.drawShoot(extraCtx, x, y);
-  }
-  checkCollision(enemy, extraCtx, heightMax) {
-    var i;
-    for (i = 0; i < enemy.length; i++) {
-      if ((this.x + extraCtx.canvas.width / 150 >= enemy[i].x - enemy[i].sizeEnemy && this.x + extraCtx.canvas.width / 150 <= enemy[i].x + enemy[i].sizeEnemy) && (this.y + extraCtx.canvas.width / 150 >=
-          enemy[i].y - enemy[i].sizeEnemy && this.y - extraCtx.canvas.width / 150 <= enemy[i].y + enemy[i].sizeEnemy)) {
-        enemy.splice(i, 1);
-        return 1;
-      }
+    constructor(x, y, r) {
+        super(x, y);
     }
-  }
+    drawShoot(extraCtx, x, y) {
+        extraCtx.beginPath();
+        extraCtx.arc(x, y, extraCtx.canvas.width / 150, 0, 2 * Math.PI);
+        extraCtx.fillStyle = "#fa9731"
+        extraCtx.fill();
+    }
+    renewCOORDS(extraCtx, x, y) {
+        this.drawShoot(extraCtx, x, y);
+    }
+    checkCollision(enemy, extraCtx, heightMax) {
+        var i;
+        for (i = 0; i < enemy.length; i++) {
+            if ((this.x + extraCtx.canvas.width / 150 >= enemy[i].x - enemy[i].sizeEnemy && this.x + extraCtx.canvas.width / 150 <= enemy[i].x + enemy[i].sizeEnemy) && (this.y + extraCtx.canvas.width / 150 >=
+                    enemy[i].y - enemy[i].sizeEnemy && this.y - extraCtx.canvas.width / 150 <= enemy[i].y + enemy[i].sizeEnemy)) {
+                enemy.splice(i, 1);
+                return 1;
+            }
+        }
+    }
 }
 
 class Enemy extends Shape {
-  constructor(x, y, sizeEnemy) {
-    super(x, y);
-    this.sizeEnemy = sizeEnemy;
-  }
-  drawEnemy(enemyCtx, xEnemy, yEnemy, sizeEnemy) {
-    enemyCtx.beginPath();
-    enemyCtx.arc(xEnemy, yEnemy, sizeEnemy, 0, 2 * Math.PI);
-    enemyCtx.fillStyle = "#6a6a69"
-    enemyCtx.fill();
-  }
-  renewCOORDE(enemyCtx, xEnemy, yEnemy, sizeEnemy) {
-    this.drawEnemy(enemyCtx, xEnemy, yEnemy, sizeEnemy);
-  }
-  checkCollision(enemyCtx, spaceship,enemy) {
-    if((spaceship.x+(enemyCtx.canvas.width / 10) >= this.x-this.sizeEnemy && spaceship.x <= this.x-this.sizeEnemy)&&(spaceship.y+enemyCtx.canvas.height/10 >= this.y-this.sizeEnemy && spaceship.y <= this.y+this.sizeEnemy)){
-      if(enemy.length >0)
-        return 1;
+    constructor(x, y, sizeEnemy) {
+        super(x, y);
+        this.sizeEnemy = sizeEnemy;
     }
-  }
+    drawEnemy(enemyCtx, xEnemy, yEnemy, sizeEnemy) {
+        enemyCtx.beginPath();
+        enemyCtx.arc(xEnemy, yEnemy, sizeEnemy, 0, 2 * Math.PI);
+        enemyCtx.fillStyle = "#6a6a69"
+        enemyCtx.fill();
+    }
+    renewCOORDE(enemyCtx, xEnemy, yEnemy, sizeEnemy) {
+        this.drawEnemy(enemyCtx, xEnemy, yEnemy, sizeEnemy);
+    }
+    checkCollision(enemyCtx, spaceship, enemy) {
+        if ((spaceship.x + (enemyCtx.canvas.width / 10) >= this.x - this.sizeEnemy && spaceship.x <= this.x - this.sizeEnemy) && (spaceship.y + enemyCtx.canvas.height / 10 >= this.y - this.sizeEnemy && spaceship.y <= this.y + this.sizeEnemy)) {
+            if (enemy.length > 0)
+                return 1;
+        }
+    }
 }
